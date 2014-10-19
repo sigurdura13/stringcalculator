a511c0b08c060ba52d7fecb0a9e88ff8c98b5c84
sigurdura13 <sigurdura13@ru.is>
2014-10-19 21:16:29 +0000

    Refactor: isEmptyString boolean function

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -3,9 +3,13 @@ package is.ru.stringcalculator;
 public class Calculator {
 
 	public static int add(String text) {
-		if(text.equals("")) {
+		if(isEmptyString(text)) {
 			return 0;
 		}
 		return Integer.parseInt(text);
+	}
+
+	private static Boolean isEmptyString(String text) {
+		return text.equals("");
 	}
 }
