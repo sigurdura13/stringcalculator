319bdf358a768d5d02dffb768c734697e518ba51
sigurdura13 <sigurdura13@ru.is>
2014-10-19 21:38:41 +0000

    Created fourth test: testMultipleNumbers

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -3,12 +3,11 @@ package is.ru.stringcalculator;
 public class Calculator {
 
 	public static int add(String text) {
-		if (isEmptyString(text)) {
+		if(isEmptyString(text)) {
 			return 0;
 		}
 		else if(text.contains(",")) {
-			String[] numbers = text.split(",");
-			return toInt(numbers[0]) + toInt(numbers[1]);
+			return sum(splitNumbers(text));
 		} else {
 			return 1;
 		}
@@ -21,4 +20,16 @@ public class Calculator {
 	private static int toInt(String number) {
 		return Integer.parseInt(number);
 	}
+
+	private static String[] splitNumbers(String numbers) {
+	    return numbers.split(",");
+	}
+
+	private static int sum(String[] numbers) {
+ 	    int total = 0;
+        for(String number : numbers) {
+		    total += toInt(number);
+		}
+		return total;
+    }
 }
